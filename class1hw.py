12b4f4c462c285073be18d988dc877e7a6d37790
Douglas Devine <douglasdevine83@Gmail.com>
2019-03-06 11:02:53 -0600

    Add files via upload

---
+++ class1hw.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 09:48:13 2019
+
+@author: ddevine
+"""
+
+# class1hw.py
+
+a = 2
+b = 4
+
+# With four different print statements
+print("The Sum of",a,"and",b,"is :",a+b)
+print("The Subtraction of",a,"and",b,"is :",a-b)
+print("The Multiplication of",a,"and",b,"is :",a*b)
+print("The Division of",a,"and",b,"is :",a/b)
+
+# All done with in one single print statement
+print("The Sum of",a,"and",b,"is :",a+b,
+"\nThe Subtraction of",a,"and",b,"is :",a-b,
+"\nThe Multiplication of",a,"and",b,"is :",a*b,
+"\nThe Division of",a,"and",b,"is :",a/b)
